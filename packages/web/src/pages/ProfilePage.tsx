91c33598e492d508d1d694f34160fe20df6b3153
Jean Carlos Delgado <delgadopupojeancarlos@gmail.com>
2026-08-21 13:50:58 -0500

    fix(web): aumentar padding en cards con mucho contenido (Settings, Profile)

--- packages/web/src/pages/ProfilePage.tsx
+++ packages/web/src/pages/ProfilePage.tsx
@@ -616,7 +616,7 @@ const ProfilePage: React.FC = () => {
           <Box
             sx={{
               ...glassmorphism,
-              p: 3,
+              p: 3.5,
             }}
           >
             <Typography
